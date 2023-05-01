3b98d0aa5fe5620fc24ba7fbf45fdb591ad1cbde
Vladislav <vl_evlashin@mail.ru>
2023-05-01 17:19:49 +0300

    Add a DB

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+    .connect("mongodb+srv://vladislavevlashin:eCmiKCD2mUBkwrPv@cluster0.oe6ksvn.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log('DB OK'))
+    .catch((e) => console.log(e))
 
 const app = express();
 
